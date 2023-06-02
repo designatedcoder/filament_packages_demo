408310c83ec8ce80c4792041dc5dbc4ee933bcc2
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2023-06-02 03:07:16 -0400

    installed and configured spatie's roles and permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,8 +10,9 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
+    public function run(): void {
+        $this->call(RolesAndPermissionsSeeder::class);
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
