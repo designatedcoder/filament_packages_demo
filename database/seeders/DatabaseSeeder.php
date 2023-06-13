e25e2cb26d6bf627b00ecd09d39749a247f2a139
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2023-06-13 00:44:30 -0400

    Set up filament-socialite migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void {
-        // $this->call(RolesAndPermissionsSeeder::class);
+        $this->call(RolesAndPermissionsSeeder::class);
         \App\Models\Post::factory(50)->create();
 
         // \App\Models\User::factory(10)->create();
