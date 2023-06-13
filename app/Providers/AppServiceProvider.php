a8ffce15844d36369ed352489006cc8b7466c695
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2023-06-13 00:45:16 -0400

    Added providers to filament-socialite config file and app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\ServiceProvider;
+use DutchCodingCompany\FilamentSocialite\FilamentSocialite;
+use Laravel\Socialite\Contracts\User as SocialiteUserContract;
+use DutchCodingCompany\FilamentSocialite\Facades\FilamentSocialite as FilamentSocialiteFacade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +22,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(): void {
+        FilamentSocialiteFacade::setCreateUserCallback(fn (SocialiteUserContract $oauthUser, FilamentSocialite $socialite) => $socialite->getUserModelClass()::create([
+            'name' => $oauthUser->getName(),
+            'email' => $oauthUser->getEmail(),
+            'password' => Hash::make(Str::random(7)),
+        ]));
     }
 }
